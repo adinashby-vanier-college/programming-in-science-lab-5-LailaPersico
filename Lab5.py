55446eec97df998ac0062e1a67a2e8fb11be3b5b
laila persico <lailapersico@lailas-MacBook-Air-2.local>
2025-10-05 20:12:08 -0400

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,44 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    result = ""
+
+    for i in range (n, 0, -1):
+        if i == n or i == n - (n - 1):
+            result += (n * "*")
+            result += "\n"
+        else:
+            result += ("*" + (n - 2) * " " + "*")
+            result += "\n"
+    return result.rstrip()
+
+#print(hollow_square(5)) 
+
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    result = ""
+
+    for i in range(n):
+        for j in range(1, 2 + i):
+            result += str(j)
+        result += "\n"
+
+    return result.rstrip()
+            
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    result = 0
+    total = 0
+    for i in range(1, n + 1):
+        total += i
+
+    return total 
+
 
 # Example for n = 4:
 #    *
@@ -24,4 +50,13 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    result = ""
+
+    for i in range(n):
+        result += " " * (n - i - 1)
+        result += "*" * (2 * i + 1)
+        result += "\n"
+    return result.rstrip()
+
+    
+
